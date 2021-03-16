f22694ec3ae5acc5d6b0e798cd50bdd761bb5125
Rafael Silva <rasilva.1986@gmail.com>
2021-03-16 06:42:51 -0700

    Finished the project

--- src/main/java/com/teknikhogskolan/example_random_between_numbers/Main.java
+++ src/main/java/com/teknikhogskolan/example_random_between_numbers/Main.java
@@ -3,7 +3,27 @@ package com.teknikhogskolan.example_random_between_numbers;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		MyRandomMethods mrm = new MyRandomMethods();
+		
+		for (int i = 0; i < 30; i++) {
+			System.out.print(mrm.randomBetween1And10() +", ");
+		}
+		System.out.println("");
+		for (int i = 0; i < 30; i++) {
+			System.out.print(mrm.randomBetween11And20() +", ");
+		}
+		System.out.println("");
+		for (int i = 0; i < 30; i++) {
+			System.out.print(mrm.randomBetween21And30() +", ");
+		}
+		System.out.println("");
+		for (int i = 0; i < 30; i++) {
+			System.out.print(mrm.randomBetween31And40() +", ");
+		}
+		System.out.println("");
+		for (int i = 0; i < 30; i++) {
+			System.out.print(mrm.randomBetween41And50() +", ");
+		}		
 
 	}
 
